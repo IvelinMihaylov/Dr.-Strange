575bbe349cd921552bdcc0afdd0cf6c6f0d1b90a
Ivelin Mihaylov <ivelinmihaylov99@gmail.com>
2018-07-26 15:12:24 +0300

    add models

---
+++ src/main/java/com/drstrange/drstrange/models/Article.java
@@ -0,0 +1,73 @@
+package com.drstrange.drstrange.models;
+
+@Entity
+@Table (name = "article")
+public class Article {
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  @Column (name = "articleID")
+  private int id;
+  
+  @Column (name = "title")
+  private String title;
+  
+  @Column (name = "author")
+  private int author;
+  
+  @Column (name = "text")
+  private String text;
+  
+  @Column (name = "image")
+  private byte[] image;
+  
+  public Article() {
+  }
+  
+  public Article(String title, int author, String text, byte[] image) {
+    this.title = title;
+    this.author = author;
+    this.text = text;
+    this.image = image;
+  }
+  
+  public int getId() {
+    return id;
+  }
+  
+  public void setId(int id) {
+    this.id = id;
+  }
+  
+  public String getTitle() {
+    return title;
+  }
+  
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  
+  public int getAuthor() {
+    return author;
+  }
+  
+  public void setAuthor(int author) {
+    this.author = author;
+  }
+  
+  public String getText() {
+    return text;
+  }
+  
+  public void setText(String text) {
+    this.text = text;
+  }
+  
+  public byte[] getImage() {
+    return image;
+  }
+  
+  public void setImage(byte[] image) {
+    this.image = image;
+  }
+}
+
