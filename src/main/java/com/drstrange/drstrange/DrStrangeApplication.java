01952f4d194873f259caca47fca9b3701c9438a4
Ivelin Mihaylov <ivelinmihaylov99@gmail.com>
2018-07-26 14:10:02 +0300

    add project

---
+++ src/main/java/com/drstrange/drstrange/DrStrangeApplication.java
@@ -0,0 +1,12 @@
+package com.drstrange.drstrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrStrangeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DrStrangeApplication.class, args);
+  }
+}
